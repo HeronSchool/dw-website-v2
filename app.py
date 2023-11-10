3a0599cf8fdcc90ac83b25f285f18cc56b4efca1
HeronSchool <lid09270411@gmail.com>
2023-11-10 04:46:46 +0000

    add dynamic data and api route

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+PROJECTS = [
+  {
+    'id': 1,
+    'language': 'C/C++',
+    'description': 'Algorithm & Programming',
+    'topic': 'Searching, Sorting, Data Structure'
+  },
+  {
+    'id': 2,
+    'language': 'Java',
+    'description': 'Android & Smartphone Application Development',
+    'topic': '공공데이터포털(Open API), GPS'
+  },
+  {
+    'id': 3,
+    'language': 'Python',
+    'description': 'Machine Learning & Data Science',
+  },
+  {
+    'id': 4,
+    'language': 'C#',
+    'description': 'Unity & Game Development',
+    'topic': 'Muscle Synergy, EMG Data Report'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', projects=PROJECTS, Author_name="DW")
+
+@app.route("/api/projects")
+def list_projects():
+  return jsonify(PROJECTS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
